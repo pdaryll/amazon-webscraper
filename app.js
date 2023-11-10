96755a1ad276b352ccfd27d97a4c0bf5b04cccac
Daryll <77931825+pdaryll@users.noreply.github.com>
2023-11-10 19:36:09 +0800

    Make default endpoint point to html file.
    Created an endpoint which will send the required html file and
    automatically opens it in a browser. Is equivalent to opening the
    html file directly. Also added some useful info in the console when
    running the server.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const {fetchAmazonSearchResults, extractProductDetails} = require('./scraper');
 
 const app = express();
 const port = 3000;
 
+// This will make the default endpoint return the required html file and view it
+// directly in a browser for easy viewing. This will only work if there is a folder
+// named "public" with an "index.html".
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 // Fetching the endpoint from the script.js results in it being 
 // blocked by the cors policy so we must add "Access Control Allow Origin" 
-// in the headers.But sometimes running the code without this is perfectly
+// in the headers. But sometimes running the code without this is perfectly
 // fine for some reason. 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -32,9 +41,12 @@ app.get('/api/scrape', async (req, res) => {
     } else {
       res.status(500).json({ error: 'Error scraping Amazon' });
     }
-  });
+});
 
-  app.listen(port, () => {
+// To be shown in the console/terminal.
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+    console.log("Use Crtl+C to turn off server")
+    console.log("Open page here: http://localhost:3000")
+});
   
